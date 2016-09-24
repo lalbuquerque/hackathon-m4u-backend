6c95aef0319d4c505764ad01f5ea5a4ab65f660c
rodrigohenriques <rsh.trabalho@gmail.com>
2016-09-24 18:25:54 -0300

    adding middleware to validate requests

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Request: ' + req.toString());
+    console.log('Response: ' + res.toString());
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     var packageJson = require('./package.json');
